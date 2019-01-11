6769837335d69f39807e772514690755096087b7
cloverrose <clover.plus.rose@gmail.com>
2019-01-11 16:18:38 +0900

    Add GetObjects API (#13)
    * implement getobjects http interface
    
    * change vector type to []float32, refine error message
    
    * remove unnecessary verctor copy
    
    * refine error message and efficient formatting
    
    * fix unit test

--- service/service.go
+++ service/service.go
@@ -17,6 +17,7 @@
 package service
 
 import (
+	"fmt"
 	"sync"
 
 	"github.com/yahoojapan/gongt"
@@ -31,6 +32,11 @@ type SearchResult struct {
 	Id       []byte
 	Distance float32
 	Error    error
+}
+
+type GetObjectResult struct {
+	Id     []byte
+	Vector []float32
 }
 
 var (
@@ -140,4 +146,27 @@ func (s *Service) Remove(id []byte) error {
 	}
 
 	return s.db.Delete(id)
+}
+
+func GetObject(id []byte) (*GetObjectResult, error) {
+	return s.GetObject(id)
+}
+
+func (s *Service) GetObject(id []byte) (*GetObjectResult, error) {
+	in, err := s.db.GetVal(id)
+	if err != nil {
+		return nil, err
+	}
+	if in <= 0 {
+		return nil, fmt.Errorf("Id(%s) is not in DB error", id)
+	}
+	vector, err := gongt.GetStrictVector(in)
+	if err != nil {
+		return nil, err
+	}
+	ret := &GetObjectResult{
+		Id:     id,
+		Vector: vector,
+	}
+	return ret, nil
 }
